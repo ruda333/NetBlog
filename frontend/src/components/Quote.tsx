eb06d2379559ebb887d4979d49ce10ccb060ffcc
hrudayvv <hrudayvv@gmail.com>
2024-01-14 15:31:59 +0530

    added the frontend and updated

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,20 @@
+export const Quote = () => {
+    return (
+        <div className="bg-slate-200 h-screen flex justify-center items-center flex-col">
+            <div className="flex justify-center">
+                <div className="max-w-lg text-3xl font-bold text-center">
+                    <div className="text-3xl font-semibold">
+                        "Good writing is supposed to evoke sensation in the reader—not the fact that it is raining, but the feeling."
+                    </div>
+                    <div className="max-w-lg text-xl font-semibold mt-4">
+                        — E.L. Doctorow
+                    </div>
+                    <div className="max-w-lg text-sm font-light text-slate-700">
+                        Novelist
+                    </div>
+                </div> 
+            </div>    
+        </div>
+    );
+    
+}
